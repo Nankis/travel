4a025961e9ce67b317c3029bab86bbedb02afc68
Panax <1003863005@qq.com>
2018-04-19 21:26:39 +0800

    VueTravel_Header

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <swiper :options="swiperOption">
+        <!-- slides -->
+        <swiper-slide>I'm Slide 1</swiper-slide>
+        <swiper-slide>I'm Slide 2</swiper-slide>
+        <!-- Optional controls -->
+        <div class="swiper-pagination"  slot="pagination"></div>
+      </swiper>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {}
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
