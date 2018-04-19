a32d787993bd5904df89cb15548919455993f532
Panax <1003863005@qq.com>
2018-04-19 22:09:45 +0800

    'change'

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,9 +1,11 @@
 <template>
-    <div>
+    <div class="wrapper">
       <swiper :options="swiperOption">
         <!-- slides -->
-        <swiper-slide>I'm Slide 1</swiper-slide>
-        <swiper-slide>I'm Slide 2</swiper-slide>
+        <swiper-slide v-for="item in swiperList" :key="item.id">
+          <img class="swiper-img"
+          :src="item.imgUrl">
+        </swiper-slide>
         <!-- Optional controls -->
         <div class="swiper-pagination"  slot="pagination"></div>
       </swiper>
@@ -16,12 +18,34 @@ export default {
   name: 'HomeSwiper',
   data () {
     return {
-      swiperOption: {}
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/15/9250dbc86a456302.jpg_640x200_4c7220d4.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/93/ac12bace75b5fc02.jpg_640x200_be20b821.jpg'
+        }
+      ]
     }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  .wrapper >>> .swiper-pagination-bullet-active
+    background :#ffffff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background :#eee
+    .swiper-img
+      width: 100%
 </style>
